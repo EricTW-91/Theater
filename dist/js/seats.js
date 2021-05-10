638ea05834a15d350885a00f63cc9fbedac5f671
Eric Tseng <amperic05@cengguanyudeMacBook-Pro.local>
2021-05-10 13:47:38 -0700

    final updated

--- dist/js/seats.js
+++ dist/js/seats.js
@@ -6,11 +6,37 @@ let ticketPrice = 12;
 
 $(document).ready(()=>{
     if(fromHomepage == "true"){
-        $('.movieContainer').append(`<h2>Hello!!</h2>`);
+        $('.movieContainer').append(`<label for="movies">Select a movie: </label><select name="movies" id="movies"></select>`);
+        fetchData(apiObj.movie.now_playing, apiObj.apiKey, apiObj.language, '', '1')
+        .then(data => {
+                    for(i=0; i<10; i++){
+                        $('#movies').append(`<option value="${i}">${data.results[i].title}</option>`)
+
+                    }
+                })
     }else{
         $('.movieContainer').append(`<h2>${movieTitle}</h2>`);
     }
 })
+
+let apiObj = {
+    apiKey: 'aebea828d86787cf050a2a920275c0ec',
+    language: 'en-US',
+    movie: {
+        popular: 'movie/popular',
+        now_playing: 'movie/now_playing',
+    }
+    
+}
+
+function fetchData(url = '', apiKey = '', lan = '', movie_id ='', page){
+
+    return fetch(`https://api.themoviedb.org/3/${url}?api_key=${apiKey}&language=${lan}&page=${page}`)
+        .then(res => res.json())
+        .then(data => {
+            return data;
+        })
+}
 
 function seatSelected(e){
     let seatID = document.getElementById(e.id);
@@ -18,15 +44,13 @@ function seatSelected(e){
     if(seatID.classList.contains('selected')){
         seatID.classList.remove('selected');
         seatCount--;
-    }else if(!seatID.classList.contains('selected')){
+    }else if(!seatID.classList.contains('selected') && !seatID.classList.contains('occupied')){
         seatID.classList.add('selected');
         seatCount++;
     }
 
-    const countID = document.getElementById('seatCount');
-    countID.innerHTML = seatCount;
-    const totalID = document.getElementById('total');
-    totalID.innerHTML = seatCount*ticketPrice;
+    $('#seatCount').text(seatCount);
+    $('#total').text(seatCount*ticketPrice);
 }
 
 if(darkModeStatus){
