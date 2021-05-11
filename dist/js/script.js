894a070383620dc26b890e055e54bc48349f5731
Eric Tseng <amperic05@cengguanyudeMacBook-Pro.local>
2021-05-11 00:39:53 -0700

    script.js updated

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,52 +1,108 @@
 let apiObj = {
     apiKey: 'aebea828d86787cf050a2a920275c0ec',
     language: 'en-US',
-    movie: {
-        popular: 'movie/popular',
-        now_playing: 'movie/now_playing',
-    }
+    // movie: {
+    //     popular: 'movie/popular',
+    //     now_playing: 'movie/now_playing',
+    // },
+
+    now_playing(page){
+        return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${this.apiKey}&language=${this.language}&page=${page.toString()}`)
+            .then(res => res.json())
+            .then(data => {
+                return data;
+            })
+    },
     
 }
 
-function fetchData(url = '', apiKey = '', lan = '', movie_id ='', page){
-
-    return fetch(`https://api.themoviedb.org/3/${url}?api_key=${apiKey}&language=${lan}&page=${page}`)
-        .then(res => res.json())
-        .then(data => {
-            return data;
-        })
-}
-
 let pageSection = 0;
-
+const pagesPerSection = 8;
 
 $(document).ready(()=>{
-    // Get popular movies
-    fetchData(apiObj.movie.now_playing, apiObj.apiKey, apiObj.language, '', '1')
+    // Get the first page of now_playing
+    apiObj.now_playing(1)
         .then(data => {
-            // Append movie divs
-            for(i=0; i<data.results.length; i++){
-                $('#now_playing').append(`<div class="movie" id="${data.results[i].id}" onclick="openDetailPage(this)"><img src="https://image.tmdb.org/t/p/original${data.results[i].poster_path}"></div>`);
-            }
-
-
-            // Show pages button
-            // $('#pageNumbers').append(`<button class="pageBtn" onclick="pageChange()"></button>`);
-            let pageLength;
-
-            if(data.total_pages>9){
-                pageLength = 9;
-            }else{
-                pageLength = data.total_pages;
-            }
-            for(i=0; i<pageLength; i++){
-                $('#pageNumbers').append(`<button class="pageBtn" value="${i+1}" onclick="pageChange(this)">${i+1}</button>`);
-            }
-            $('#pageNumbers').append(`<button class="pageBtn" id="addTenPages" onclick="pageNumChange(this)">>></button>`);
+            getMovies(data.results);
+            pagination(data.total_pages, pageSection);
+            console.log(data.total_pages);
         })
 })
 
+function pagination(totalPages, sectionIndex){
+    $('#pageNumbers').empty();
 
+    totalPages = parseInt(totalPages);
+
+    if(totalPages <= pagesPerSection){
+        for(i=0; i<totalPages; i++){
+            $('#pageNumbers').append(`<button class="pageBtn" value="${i+1}" onclick="pageChange(this)">${i+1}</button>`);
+        }
+    }else{
+        // Get the amount of page section
+        const endSection = Math.ceil(totalPages / pagesPerSection)-1;
+
+        // PreSection button
+        if(sectionIndex > 0){
+            $('#pageNumbers').append(`<button class="pageBtn" id="preSection" onclick="sectionChange(this)"><<</button>`);
+        }
+        
+        // Pages button
+        let sectionLength = pagesPerSection;
+        let restPages = totalPages%pagesPerSection;
+
+        if(sectionIndex == endSection){
+            sectionLength = restPages;
+            if(restPages == 0){
+                sectionLength = pagesPerSection;
+            }
+        }
+
+        for(i=0; i<sectionLength; i++){
+            const pageValue = sectionIndex*pagesPerSection+i+1;
+            $('#pageNumbers').append(`<button class="pageBtn" value="${pageValue}" onclick="pageChange(this)">${pageValue}</button>`);
+        }
+
+        // NextSection Button
+        if(sectionIndex < endSection){
+            $('#pageNumbers').append(`<button class="pageBtn" id="nextSection" onclick="sectionChange(this)">>></button>`);
+        }
+    }
+}
+
+function getMovies(movies){
+    $('#now_playing').empty();
+
+    for(i=0; i<movies.length; i++){
+        $('#now_playing').append(`<div class="movie" id="${movies[i].id}" onclick="openDetailPage(this)"><img src="https://image.tmdb.org/t/p/original${movies[i].poster_path}"></div>`);
+    }
+}
+
+function pageChange(e){
+    apiObj.now_playing(e.value)
+        .then(data => {
+            getMovies(data.results)
+        })
+}
+
+function sectionChange(e){
+    $('#now_playing').empty();
+    $('#pageNumbers').empty();
+    
+    if(e.id == 'nextSection'){
+        pageSection++;
+    }else if(e.id == 'preSection'){
+        pageSection--;
+    }
+
+    apiObj.now_playing(pageSection*pagesPerSection+1)
+        .then(data => {
+            getMovies(data.results);
+            pagination(data.total_pages, pageSection);
+        })
+}
+
+// Dark mode
 let darkModeStatus = false;
 darkModeStatus = (localStorage.getItem('darkMode') == 'true');
 
@@ -61,7 +117,7 @@ function darkMode(){
 }
 
 
-
+// Open movie detail
 function openDetailPage(e){
     localStorage.setItem('darkMode', darkModeStatus)
     localStorage.setItem('id', e.id);
@@ -69,69 +125,7 @@ function openDetailPage(e){
     window.open("./detail.html", "_self");
 }
 
-function pageChange(e){
-    fetchData(apiObj.movie.now_playing, apiObj.apiKey, apiObj.language, '', e.value)
-        .then(data => {
-            $('#now_playing').empty();
-            for(i=0; i<data.results.length; i++){
-                $('#now_playing').append(`<div class="movie" id="${data.results[i].id}" onclick="openDetailPage(this)"><img src="https://image.tmdb.org/t/p/original${data.results[i].poster_path}"></div>`);
-            }
-        })
-}
-
-function pageNumChange(e){
-    $('#pageNumbers').empty();
-    
-    if(e.id == 'addTenPages'){
-        pageSection++;
-    }else if(e.id == 'subTenPages'){
-        pageSection--;
-    }
-    // console.log(e.id);
-
-    let page = pageSection*10;
-    if(pageSection == 0){
-        page = 1;
-    }
-
-    fetchData(apiObj.movie.now_playing, apiObj.apiKey, apiObj.language, '', page.toString())
-        .then(data => {
-            console.log((pageSection)*10);
-            $('#now_playing').empty();
-            for(i=0; i<data.results.length; i++){
-                $('#now_playing').append(`<div class="movie" id="${data.results[i].id}" onclick="openDetailPage(this)"><img src="https://image.tmdb.org/t/p/original${data.results[i].poster_path}"></div>`);
-            }
-
-
-            if(pageSection != 0){
-                $('#pageNumbers').append(`<button class="pageBtn" id="subTenPages" onclick="pageNumChange(this)"><<</button>`);
-            }
-
-            let pageLength;
-            if(pageSection < Math.floor(data.total_pages/10)){
-                pageLength = 9;
-            }else if(pageSection == Math.floor(data.total_pages/10)){
-                pageLength = data.total_pages % 10;
-            }
-
-            if(pageSection != 0){
-
-                $('#pageNumbers').append(`<button class="pageBtn" value="${pageSection * 10}" onclick="pageChange(this)">${pageSection * 10}</button>`);
-            }
-            for(i=0; i<pageLength; i++){
-                $('#pageNumbers').append(`<button class="pageBtn" value="${pageSection * 10 + i + 1}" onclick="pageChange(this)">${pageSection * 10 + i + 1}</button>`);
-            }
-
-            if(pageSection != Math.floor(data.total_pages/10)){
-                $('#pageNumbers').append(`<button class="pageBtn" id="addTenPages" onclick="pageNumChange(this)">>></button>`);
-            }
-        })
-
-
-
-        // console.log(pageSection);
-}
-
+// Record status of ticket page
 function openTicketPage(){
     localStorage.setItem('fromHomepage', 'true');
     window.open("./seats.html");
